6d2a24b28af737436c78a638ed009d35b5b26419
Surya <suryaputra0611@gmail.com>
2026-06-11 18:49:00 +0700

    Add files via upload

---
+++ Soal1/Main.java
@@ -0,0 +1,52 @@
+package Soal1;
+
+// Nama : Surya Putra HS
+// NIM  : 2902792001
+
+// Soal 1: Pembuatan Class dan Object
+
+// 1. Kelas Mahasiswa
+class Mahasiswa {
+    // Atribut kelas
+    String nama;
+    String nim;
+    String jurusan;
+    double ipk;
+
+    // 2. Constructor sebagai inisialisasi data mahasiswa
+    public Mahasiswa(String nama, String nim, String jurusan, double ipk) {
+        this.nama = nama;
+        this.nim = nim;
+        this.jurusan = jurusan;
+        this.ipk = ipk;
+    }
+
+    // 3. Method tampilkanInfo() untuk menampilkan data
+    public void tampilkanInfo() {
+        System.out.println("Nama    : " + this.nama);
+        System.out.println("NIM     : " + this.nim);
+        System.out.println("Jurusan : " + this.jurusan);
+        System.out.println("IPK     : " + this.ipk);
+        System.out.println("---------------------------------");
+    }
+}
+
+// Kelas Utama untuk menjalankan program
+public class Main {
+    public static void main(String[] args) {
+        // 4. 5 objek mahasiswa dengan data berbeda
+        Mahasiswa mhs1 = new Mahasiswa("Andi Pratama", "2440001", "Teknik Informatika", 3.75);
+        Mahasiswa mhs2 = new Mahasiswa("Budi Santoso", "2440002", "Sistem Informasi", 3.40);
+        Mahasiswa mhs3 = new Mahasiswa("Citra Lestari", "2440003", "Teknik Informatika", 3.90);
+        Mahasiswa mhs4 = new Mahasiswa("Joni Suhartono", "2440004", "Teknik Industri", 3.00);
+        Mahasiswa mhs5 = new Mahasiswa("Bulan Suci", "2440005", "Akuntansi", 3.20);
+
+        // Tampilkan Semua informasi mahasiswa ke konsol
+        System.out.println("==== DATA MAHASISWA ====\n");
+        mhs1.tampilkanInfo();
+        mhs2.tampilkanInfo();
+        mhs3.tampilkanInfo();
+        mhs4.tampilkanInfo();
+        mhs5.tampilkanInfo();
+    }
+}
